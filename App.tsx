e0125dd126dc87fd6135899ef441b10c365f3479
Adelblande <adelblande@gmail.com>
2022-10-16 23:05:25 -0300

    Routes separation

--- App.tsx
+++ App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 
-import { Routes } from './src/routes/app.routes';
 import theme from './src/global/styles/theme';
-import { StatusBar } from 'react-native';
+import { Routes } from './src/routes';
 import { AuthProvider } from './src/hooks/auth';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <StatusBar
-          backgroundColor={theme.colors.primary}
-          barStyle="light-content"
-        />
-        <AuthProvider>
-          <Routes />
-        </AuthProvider>
-      </NavigationContainer>
+      <StatusBar
+        backgroundColor={theme.colors.primary}
+        barStyle="light-content"
+      />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   );
 };
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Login } from '../screens/Login';
-import { Home } from '../screens/Home';
-import { Details } from '../screens/Details';
 
 const Stack = createNativeStackNavigator();
 
-export function AppRoutes() {
+export function AuthRoutes() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Details" component={Details} />
+      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
 }
